4d5b632c2c32a524bea47de3d625ad2606dfe561
Ramon Bartl <ramon_bartl@yahoo.de>
2012-01-29 19:00:40 +0100

    added Calculator Stack Stub

---
+++ Calculator/CalculatorBrain.h
@@ -0,0 +1,17 @@
+//
+//  CalculatorBrain.h
+//  Calculator
+//
+//  Created by Bartl Ramon on 29.01.12.
+//  Copyright (c) 2012 nexiles GmbH. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CalculatorBrain : NSObject
+
+// declaration
+- (void) pushOperand:(double)operand;
+- (double)performOperation:(NSString *)operation;
+
+@end
---
+++ Calculator/CalculatorBrain.m
@@ -0,0 +1,23 @@
+//
+//  CalculatorBrain.m
+//  Calculator
+//
+//  Created by Bartl Ramon on 29.01.12.
+//  Copyright (c) 2012 nexiles GmbH. All rights reserved.
+//
+
+#import "CalculatorBrain.h"
+
+@implementation CalculatorBrain
+
+- (void) pushOperand:(double)operand {
+    
+}
+
+- (double)performOperation:(NSString *)operation {
+    double result = 0;
+    // calculate result
+    return result;
+}
+
+@end
